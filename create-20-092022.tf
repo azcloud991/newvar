5965226300b73a4f1d83ccf6ec42e4818a7ff221
vardhanarao <pamula.rao@globallogic.com>
2022-09-20 22:49:24 +0530

    added

---
+++ create-20-092022.tf
@@ -0,0 +1,28 @@
+provider "github" {
+    token = "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"
+
+
+}
+
+data "github_repository" "example" {
+  name        = "newvar"
+}
+
+# Protect the main branch of the foo repository. Only allow a specific user to merge to the branch.
+resource "github_branch_protection" "example" {
+  repository_id       = "${data.github_repository.example.name}"  
+  pattern             = "main"
+  enforce_admins      = true
+  
+
+  required_status_checks {
+    strict   = false
+    contexts = ["delete"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews  = false
+    restrict_dismissals    = false
+  }
+
+}
